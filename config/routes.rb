857539a60fe29137b065b0271b26ae37ffecff0c
andres-soto-h <andres.soto.h@outlook.com>
2018-08-08 20:56:10 -0500

    Añado las nuevas rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/about_us'
+  get 'pages/contact'
+  get 'pages/post'
+
 end
