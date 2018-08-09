5adb8e08c9f234e4299c9ca09b778203fad1486e
andres-soto-h <andres.soto.h@outlook.com>
2018-08-08 20:55:51 -0500

    Agrego las paginas al controlador

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about_us
+    @saludo="Hola Andrés"
+  end
+
+  def contact
+    @saludo="Estas en el contact"
+  end
+
+  def post
+    @saludo="Estas en post"
+  end
+
+  #about_us
+
+  #contact
+
+  #posts
+
 end
