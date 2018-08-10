7a1c7e5fd8c10b09e7d0cfb7a7e481a61ea2dfd1
andres-soto-h <andres.soto.h@outlook.com>
2018-08-09 22:30:06 -0500

    mejoras espacios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,27 @@
-class PagesController < ApplicationController
+# frozen_string_literal: true
 
-layout "custom_layout", only: [:hello] #Define un custom layout para este controlador
+class PagesController < ApplicationController
+  layout 'custom_layout', only: [:hello] # Define un custom layout para este controlador
 
   def home
-    #render html: "<h1>Hola Andrés desde home</h1>".html_safe
+    # render html: "<h1>Hola Andrés desde home</h1>".html_safe
   end
 
   def about_us
-    @saludo="Hola Andrés"
+    @saludo = 'Hola Andrés'
   end
 
   def contact
-    @saludo="Estas en el contact"
+    @saludo = 'Estas en el contact'
   end
 
   def hello
-    @name=params[:name]
+    @name = params[:name]
   end
 
   def post
-    @posts=Post.all
-    #render layout: "custom_layout"
+    @posts = Post.all
+    # render layout: "custom_layout"
+    redirect_to hello_path('Andres Soto')
   end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-
   def index
-    @posts=Post.all
-    @var="Estas en el controlador de Post"
+    @posts = Post.all
+    @var = 'Estas en el controlador de Post'
   end
-
 end
