05bc6eebb363414a80134a7aa1147ae807e606cd
andres-soto-h <andres.soto.h@outlook.com>
2018-08-13 18:44:20 -0500

    add new user table and create foreign key to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_014301) do
+ActiveRecord::Schema.define(version: 2018_08_13_234318) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2018_08_11_014301) do
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -34,6 +37,14 @@ ActiveRecord::Schema.define(version: 2018_08_11_014301) do
     t.string "phone_number"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
