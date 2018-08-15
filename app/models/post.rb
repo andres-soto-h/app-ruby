82877e5495ca75d75d6ddad22df2372cb099ed17
andres-soto-h <andres.soto.h@outlook.com>
2018-08-14 20:03:35 -0500

    Generate polymorfic model for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_many :comments, as: :commentable
     belongs_to :user
     validates :title, :author, presence: true
     validates :published, inclusion: { in: [true, false] }
