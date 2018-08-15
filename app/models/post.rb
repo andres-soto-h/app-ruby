4d3bb10438542ea5b5cfe5f76a55ec483c056ccd
andres-soto-h <andres.soto.h@outlook.com>
2018-08-14 20:56:10 -0500

    scope examples

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
     belongs_to :user
     validates :title, :author, presence: true
     validates :published, inclusion: { in: [true, false] }
+    scope :published, -> {where(published: true)}
+    scope :created_before, -> (time) {where("created_at <?", time)}
+
+    # def self.published
+    #     where(published: true)
+    # end
 end
